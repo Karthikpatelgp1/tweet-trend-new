56aba2f432efe064cce121dfe313a11844b6b912
Karthikpatelgp1 <138849637+Karthikpatelgp1@users.noreply.github.com>
2023-11-22 12:44:12 +0530

    Update DemoWorkshopApplication.java

--- src/main/java/com/valaxy/demo/DemoWorkshopApplication.java
+++ src/main/java/com/valaxy/demo/DemoWorkshopApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoWorkshopApplication{
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(DemoWorkshopApplication.class, args);
 	}
 
